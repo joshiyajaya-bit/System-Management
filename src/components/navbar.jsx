23d421604713a8b885abd6e03710757e3a3d2f62
joshiyajaya-bit <joshiyajaya@gmail.com>
2026-07-02 18:26:43 +0530

    adjust

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -35,19 +35,6 @@ function Navbar() {
       {/* Right */}
       <div className="flex items-center gap-5">
 
-        {/* Search
-        <div className="relative">
-
-          <FaSearch className="absolute left-3 top-3 text-gray-400" />
-
-          <input
-            type="text"
-            placeholder="Search..."
-            className="w-64 pl-10 pr-4 py-2 border rounded-xl outline-none
-                       focus:ring-2 focus:ring-blue-500"
-          />
-
-        </div> */}
 
         {/* Notification */}
         <button className="relative text-xl text-gray-600 hover:text-blue-600">
