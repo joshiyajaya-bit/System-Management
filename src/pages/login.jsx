293ffc9fd04a5b0f3dd07a18ca578648dba74f26
joshiyajaya-bit <joshiyajaya@gmail.com>
2026-07-03 16:49:10 +0530

    login 1

--- src/pages/login.jsx
+++ src/pages/login.jsx
@@ -1,17 +1,36 @@
+import { useState } from "react";
 import { useNavigate } from "react-router-dom";
+import users from "../data/users.json";
 
 export default function Login() {
   const navigate = useNavigate();
 
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState("");
+
   const handleLogin = () => {
-    localStorage.setItem("isLoggedIn", "true");
-    navigate("/");
+    const user = users.find(
+      (u) =>
+        u.username === username &&
+        u.password === password
+    );
+
+    if (user) {
+      localStorage.setItem("isLoggedIn", "true");
+      localStorage.setItem("username", user.username);
+      localStorage.setItem("role", user.role);
+
+      navigate("/");
+    } else {
+      setError("Invalid Username or Password");
+    }
   };
 
   return (
     <div className="min-h-screen flex items-center justify-center bg-slate-100">
 
-      <div className="bg-white shadow-xl rounded-2xl p-10 w-[400px]">
+      <div className="bg-white p-10 rounded-2xl shadow-xl w-[400px]">
 
         <h1 className="text-3xl font-bold text-center mb-8">
           JKNS Login
@@ -20,18 +39,28 @@ export default function Login() {
         <input
           type="text"
           placeholder="Username"
-          className="w-full border rounded-lg p-3 mb-4"
+          className="w-full border p-3 rounded-lg mb-4"
+          value={username}
+          onChange={(e) => setUsername(e.target.value)}
         />
 
         <input
           type="password"
           placeholder="Password"
-          className="w-full border rounded-lg p-3 mb-6"
+          className="w-full border p-3 rounded-lg mb-3"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
         />
+
+        {error && (
+          <p className="text-red-500 mb-3">
+            {error}
+          </p>
+        )}
 
         <button
           onClick={handleLogin}
-          className="w-full bg-blue-600 text-white py-3 rounded-lg hover:bg-blue-700"
+          className="w-full bg-blue-600 hover:bg-blue-700 text-white py-3 rounded-lg"
         >
           Login
         </button>
